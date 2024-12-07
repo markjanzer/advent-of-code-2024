bb888ed3ebde72b3bdef27c8682e9365ad617e83
Mark Janzer <mark.janzer@gmail.com>
2024-12-06 16:00:35 -0800

    First attempt at day 6 part 2

--- day6/main.go
+++ day6/main.go
@@ -40,16 +40,30 @@ type Point struct {
 }
 
 type Direction struct {
-	X int
-	Y int
+	X    int
+	Y    int
+	Name string
 }
 
-var north = Direction{X: 0, Y: -1}
-var east = Direction{X: 1, Y: 0}
-var south = Direction{X: 0, Y: 1}
-var west = Direction{X: -1, Y: 0}
+var north = Direction{X: 0, Y: -1, Name: "N"}
+var east = Direction{X: 1, Y: 0, Name: "E"}
+var south = Direction{X: 0, Y: 1, Name: "S"}
+var west = Direction{X: -1, Y: 0, Name: "W"}
 
-var clockwise = []Direction{north, east, south, west}
+func (d Direction) TurnRight() Direction {
+	switch d.Name {
+	case "N":
+		return east
+	case "E":
+		return south
+	case "S":
+		return west
+	case "W":
+		return north
+	default:
+		return d
+	}
+}
 
 func (p Point) IsInGrid(xMax, yMax int) bool {
 	return p.X >= 0 && p.X < xMax && p.Y >= 0 && p.Y < yMax
@@ -79,14 +93,13 @@ func solvePart1(input string) int {
 	guard, obstructions, xMax, yMax := scanGrid(input)
 	visited := make(map[Point]bool)
 	visited[guard] = true
-	directionIndex := 0
+	direction := north
 
 	for guard.IsInGrid(xMax, yMax) {
 		visited[guard] = true
-		direction := clockwise[directionIndex]
 		nextPosition := Point{guard.X + direction.X, guard.Y + direction.Y}
 		if obstructions[nextPosition] {
-			directionIndex = (directionIndex + 1) % len(clockwise)
+			direction = direction.TurnRight()
 		} else {
 			guard = nextPosition
 		}
@@ -98,20 +111,52 @@ func solvePart1(input string) int {
 /*
 	Part 2 Notes
 
+	Create a newObstruction map
+	So instead of just putting true as visited, we'll put the direction name
+	If turning right at any point would be a duplicate, then we save that
+	space as a potential newObstruction
+
+	Oh the problem is that there are times when a loop would form but it still
+	is getting a unique point/direction.
 */
 
 func solvePart2(input string) int {
-	return 0
+	guard, obstructions, xMax, yMax := scanGrid(input)
+	newObstructions := make(map[Point]bool)
+	visited := make(map[Point]map[string]bool)
+	direction := north
+	visited[guard] = make(map[string]bool)
+	visited[guard][direction.Name] = true
+
+	for guard.IsInGrid(xMax, yMax) {
+		if visited[guard] == nil {
+			visited[guard] = make(map[string]bool)
+		}
+		visited[guard][direction.Name] = true
+		nextPosition := Point{guard.X + direction.X, guard.Y + direction.Y}
+		if obstructions[nextPosition] {
+			direction = direction.TurnRight()
+		} else {
+			if visited[guard] != nil && visited[guard][direction.TurnRight().Name] {
+				newObstructions[nextPosition] = true
+			}
+			guard = nextPosition
+		}
+	}
+
+	fmt.Println(obstructions)
+	fmt.Println(newObstructions)
+	return len(newObstructions)
 }
 
 func main() {
 	lib.AssertEqual(41, solvePart1(TestString))
-	// lib.AssertEqual(1, solvePart2(TestString))
+	lib.AssertEqual(6, solvePart2(TestString))
 
-	dataString := lib.GetDataString()
+	// dataString := lib.GetDataString()
 
-	result1 := solvePart1(dataString)
-	fmt.Println(result1)
+	// result1 := solvePart1(dataString)
+	// fmt.Println(result1)
 
 	// result2 := solvePart2(dataString)
 	// fmt.Println(result2)
